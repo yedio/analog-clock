d73d18d1401e8d80a53e6711f8e17a9ab4f64537
yedio <yediobjet@gmail.com>
2023-02-19 00:00:08 +0900

    setting: eslint&prettier, router, styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Router from './Router';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/globalStyles';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+const color = {
+  main: '#14DBBA',
+  gray: '#5D5D5D',
+  lightgray: '#aaaaaa',
+  dark: '#242424',
+};
+
+export const theme: DefaultTheme = { color };
